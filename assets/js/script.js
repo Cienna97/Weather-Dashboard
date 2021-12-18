8518f59e8c98c7bb583dc8a791d60ffe04387457
Cienna97 <cmortenson97@gmail.com>
2021-12-18 13:04:54 -0800

    adding variables to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,32 @@
-var searchHistory = [];
-var weatherApiRootUrl = 'https://api.openweathermap.org';
+
+var searchBar = document.getElementById('searchBar');
+var searchBtn = document.getElementById('searchBtn');var weatherApiRootUrl = 'https://api.openweathermap.org';
 var weatherApiKey = 'f6e7949c7164702382c89036e588b531';
 
 var userForm = document.querySelector('#user-form');
 var userInput = document.querySelector('#user-input');
 var todayContainer = document.querySelector('#today');
 var weatherContainer = document.querySelector('#weather');
-var searchHistoryContainer = document.querySelector('#history');
+
+cityName = document.getElementById('city-name')
+currentTemp = document.getElementById('temp')
+
+var dateOne = document.getElementById('day-one-date');
+var dateTwo = document.getElementById('day-two-date');
+var dateThree = document.getElementById('day-three-date');
+var dateFour = document.getElementById('day-four-date');
+var dateFive = document.getElementById('day-five-date');
+var tempOne = document.getElementById('one-temp');
+var tempTwo = document.getElementById('two-temp');
+var tempThree = document.getElementById('three-temp');
+var tempFour = document.getElementById('four-temp');
+var tempFive = document.getElementById('five-temp');
+
+var searchHistoryContainer = document.getElementById('search-history');
+var pastCities  =  JSON.parse(localStorage.getItem("Search-History")) || []; 
+
+
+
 
 //btn.setAttribute('info-search', searchHistory[i]);
 //btn.textContent = searchHistory[i];
@@ -15,7 +35,7 @@ var searchHistoryContainer = document.querySelector('#history');
 localStorage.setItem("Search-History", JSON.stringify(history))
 
 
-searchHistory
+
 
 var displayWeather = function(weather) {
   console.log(weather);
@@ -32,7 +52,7 @@ function renderItems(city, data) {
 
 
 function fetchWeather(searchedCity) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&units=imperial&appid=6e7949c7164702382c89036e588b531`)
+    fetch(`${weatherApiRootUrl}/data/2.5/weather?q=${searchedCity}&units=imperial&appid=${weatherApiKey}`)
     
       .then(function (res) {
         return res.json();
@@ -43,7 +63,7 @@ function fetchWeather(searchedCity) {
 
         currentWeather(data, searchedCity);
 
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=current,minutely,hourly,alerts&units=imperial&appid=6e7949c7164702382c89036e588b531`)
+        fetch(`${weatherApiRootUrl}/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=current,minutely,hourly,alerts&units=imperial&appid=${weatherApiKey}`)
         
         .then(function (response) {
           return response.json
@@ -52,7 +72,11 @@ function fetchWeather(searchedCity) {
           console.log(data)
 
           currentUVIndex.innerHTML = 'UV Index: ${data.daily[0].uvi}';
-        })
+
+
+          weatherForecast(data, 1, dateOne, symbolOne, tempOne, windOne, humidityOne);
+  
+        });
       })
    
   }
